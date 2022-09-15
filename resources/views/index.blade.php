3df298e1bd45f3199620d315a2e533be7412c20a
Zura Moon <zuramoon01@gmail.com>
2022-09-16 06:11:05 +0700

    Load data post to index view dynamically

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sosmed | Home</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
@@ -15,84 +16,64 @@
             <header class='header'>
                 <h2 class="title">Pos</h2>
                 <p class="user">
-                    {{ strtoupper('Zura') }}
+                    {{ strtoupper(auth()->user()->name) }}
                     <span class="separator"></span>
                     <a href="/logout" class="logout">Keluar</a>
                 </p>
             </header>
 
             <div class="container-post">
-                {{-- <div class="create-post-button">
+                <div class="create-post-button">
                     <img src="{{ asset('icons/plus-solid.svg') }}" alt="Ikon Tambah" class="create-post-icon">
                     <p class="create-post-text">Buat Pos</p>
-                </div> --}}
+                </div>
 
-                <form action="#" class="create-post-form">
+                <form action="/posts" class="create-post-form" method="post">
+                    @csrf
+
                     <textarea name="post" id="post" cols="50" rows="5" class="textarea"></textarea>
 
                     <button class="submit">Buat Pos</button>
                 </form>
 
-                <article class="single-post">
-                    <h3 class="author">{{ strtoupper('Zura') }}</h3>
+                @foreach ($posts as $post)
+                    <article class="single-post">
+                        <h3 class="author">{{ strtoupper($post->user->name) }}</h3>
 
-                    <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia delectus
-                        enim
-                        repellat
-                        repudiandae inventore. Excepturi quo eos velit consectetur ex.</p>
+                        <p class="content">{{ $post->post }}</p>
 
-                    <p class="date">{{ strtoupper('September 15, 2022 10:37 WIB') }} | <a href="#"
-                            class="comment">Komen</a>
-                    </p>
+                        <p class="date">{{ strtoupper($post->created_at->format('F d, Y G:i')) }}
+                            WIB |
+                            <a href="#" class="comment">Komen</a>
+                        </p>
 
-                    <form action="#" class="create-comment-form">
-                        <textarea name="comment" id="comment" cols="50" rows="3" class="textarea"></textarea>
+                        <form action="#" class="create-comment-form">
+                            <textarea name="comment" id="comment" cols="50" rows="3" class="textarea"></textarea>
 
-                        <button class="submit">Buat Komen</button>
-                    </form>
+                            <button class="submit">Buat Komen</button>
+                        </form>
 
-                    <div class="container-comment">
-                        <article class="single-comment">
-                            <h3 class="author">{{ strtoupper('Zura') }}</h3>
+                        <div class="container-comment">
+                            <article class="single-comment">
+                                <h3 class="author">{{ strtoupper('Zura') }}</h3>
 
-                            <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-                                delectus
-                                enim
-                                repellat
-                                repudiandae inventore. Excepturi quo eos velit consectetur ex.</p>
+                                <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+                                    delectus
+                                    enim
+                                    repellat
+                                    repudiandae inventore. Excepturi quo eos velit consectetur ex.</p>
 
-                            <p class="date">{{ strtoupper('September 15, 2022 10:37 WIB') }}</p>
-                        </article>
-
-                        <article class="single-comment">
-                            <h3 class="author">{{ strtoupper('Zura') }}</h3>
-
-                            <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-                                delectus
-                                enim
-                                repellat
-                                repudiandae inventore. Excepturi quo eos velit consectetur ex.</p>
-
-                            <p class="date">{{ strtoupper('September 15, 2022 10:37 WIB') }}</p>
-                        </article>
-                    </div>
-                </article>
-
-                <article class="single-post">
-                    <h3 class="author">{{ strtoupper('Zura') }}</h3>
-
-                    <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia delectus
-                        enim
-                        repellat
-                        repudiandae inventore. Excepturi quo eos velit consectetur ex.</p>
-
-                    <p class="date">{{ strtoupper('September 15, 2022 10:37 WIB') }} | <a href="#"
-                            class="comment">Komen</a>
-                    </p>
-                </article>
+                                <p class="date">{{ strtoupper('September 15, 2022 10:37 WIB') }}</p>
+                            </article>
+                        </div>
+                    </article>
+                @endforeach
             </div>
         </div>
     </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script type="module" src="{{ asset('js/index.js') }}"></script>
 </body>
 
 </html>
